19e2ca7a3c6a70112ac5badc6a9b0044d466004e
Jose-Basulto <A24216358@alumnos.uady.mx>
2026-09-22 16:39:23 -0600

    se ubicó Main en la capa presentation

--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -1,7 +1,8 @@
+package presentation;
+
 import business.OrderService;
 import data.OrderRepository;
 import data.OrderRepositoryMemory;
-import presentation.OrderUI;
 
 public class Main {
     public static void main(String[] args) {
